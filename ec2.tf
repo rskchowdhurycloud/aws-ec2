663876ac23f4c894c71ded6bb197cad4590e0822
rskchowdhurycloud <76244205+rskchowdhurycloud@users.noreply.github.com>
2021-10-22 15:15:46 +0530

    New-File

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "myawsserver" {
+  ami = "ami-0603cbe34fd08cb81"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "Suresh-Techlanders-aws-ec2-instance-v4 from github"
+    Env = "Dev"
+  }
+}
+
+output "myawsserver-ip" {
+  value = aws_instance.myawsserver.public_ip
+}
+
+output "myserver-PrivateIP" {
+  value = aws_instance.myawsserver.private_ip
+}

1b395c5a0dbe07f5de51a81d7330937baeee403a
rskchowdhurycloud <76244205+rskchowdhurycloud@users.noreply.github.com>
2021-10-22 15:23:34 +0530

    Update ec2.tf
    Added key

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "myawsserver" {
   ami = "ami-0603cbe34fd08cb81"
   instance_type = "t2.nano"
+  access_key = "AKIAZ3S6MYW233SUUS5J"
+  secret_key = "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"
 
   tags = {
     Name = "Suresh-Techlanders-aws-ec2-instance-v4 from github"
